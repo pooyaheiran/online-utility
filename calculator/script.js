63dc2b0812a08811e312969e50f72395540e8a42
pooya <pooya@localhost.localdomain>
2025-06-04 14:12:10 +0330

    first commit

---
+++ calculator/script.js
@@ -0,0 +1,17 @@
+function display(value) {
+    
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay(){
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    let display = document.getElementById("display")
+    try{
+        display.value = eval(display.value)
+    } catch {
+        display.value = "syntax ERROR"
+    }
+}
